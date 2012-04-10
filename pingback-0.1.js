fd9fb649f49a58a5d8adf2b11bf0731bca4b3860
Michele Memoli <michele.memoli@gmail.com>
2012-04-10 20:21:37 +0100

    Added some scaffolding

--- pingback-0.1.js
+++ pingback-0.1.js
@@ -1,8 +1,48 @@
 (function($) {
 
-	JQuery.fn.pingback = function() {
+	JQuery.fn.pingback = function($element, endClassName, options) {
 
-		// Wills to write stuff here
+		/**
+		* Store reference to the HTML to animate
+		*/
+		var $el = $element;
+		
+		/**
+		* Store reference to the class name that determines the
+		* endpoint of the transition
+		*/
+		var tagNameEnd = className;
+		
+		// **********
+		// Defaults
+		// **********
+		
+		/**
+		* Threshold velocity in pixels per second over which the
+		* transition automatically animates to the endpoint.
+		* default: 10 p/s
+		*/
+		var v_t = 10.0;
+		
+		/**
+		* Threshold position over which the transition automatically
+		* animates to the endpoint.
+		* default: 50%;
+		*/
+		var v_s = 50.0;
+
+
+		if (options !== undefined) {
+			// Override defaults defined above
+		}
+
+		// **********
+		// Hooks
+		// **********
+		
+		// **********
+		// Handlers
+		// **********
 
 	};
 
