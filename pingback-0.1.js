3fd39f1765f3eee3d126b497175dd6634bd29a86
Michele Memoli <michele@onehundredshapes.com>
2012-04-17 17:03:37 +0200

    Collect styles to transition to.

--- pingback-0.1.js
+++ pingback-0.1.js
@@ -4,54 +4,63 @@
 
 		pingback: function($element, endClassName, options) {
 
-		/**
-		* Store reference to the HTML to animate
-		*/
-		var $el = $element;
-		
-		/**
-		* Store reference to the class name that determines the
-		* endpoint of the transition
-		*/
-		var tagNameEnd = className;
-		
-		// **********
-		// Defaults
-		// **********
-		
-		/**
-		* Threshold velocity in pixels per second over which the
-		* transition automatically animates to the endpoint.
-		* default: 10 p/s
-		*/
-		var v_t = 10.0;
-		
-		/**
-		* Threshold position over which the transition automatically
-		* animates to the endpoint.
-		* default: 50%;
-		*/
-		var v_s = 50.0;
+			/**
+			* Store reference to the HTML to animate
+			*/
+			var $el = $element;
 
-		var defaults = {
-			v_t: v_t,
-			v_s: v_s,
-		}
-		var options =  $.extend(defaults, options);
+			/**
+			* Store reference to the class name that determines the
+			* endpoint of the transition
+			*/
+			var tagNameEnd = className;
+			
+			// **********
+			// Defaults
+			// **********
+			var defaults = {
+				/**
+				* Threshold velocity in pixels per second over which the
+				* transition automatically animates to the endpoint.
+				* default: 10 p/s
+				*/
+				v_t: 10.0,
+			
+				/**
+				* Threshold position over which the transition automatically
+				* animates to the endpoint.
+				* default: 50%;
+				*/
+				v_s = 50.0,
 
-		// **********
-		// Hooks
-		// **********
+				from: $el.position()
+			}
+
+			var options =  $.extend(defaults, options);
+
+			/**
+			* Find out where we're transitioning to
+			*/
+			var toStyles = window.getComputedStyle($el, endClassName);
+			
+			options.to = {
+				top: toStyles.getPropertyValue('top'),
+				left: toStyles.getPropertyValue('left')
+			};
+
+
+			// **********
+			// Hooks
+			// **********
+			
+			// **********
+			// Handlers
+			// **********
+
+
+		};
 		
-		// **********
-		// Handlers
-		// **********
-
-	};
-
-
 	});
-
 
 })(JQuery);
 
